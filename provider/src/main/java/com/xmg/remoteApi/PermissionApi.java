7144d36486e97b66de22532f5a62b28d309d15de
makui <kuimgg123@gmail.com>
2023-01-12 22:04:01 +0800

    。

--- provider/src/main/java/com/xmg/remoteApi/PermissionApi.java
+++ provider/src/main/java/com/xmg/remoteApi/PermissionApi.java
@@ -4,7 +4,7 @@ import com.xmg.support.Permission;
 import xmg.client.support.RpcApi;
 
 //@RpcApi("provider2")
-@RpcApi(url = "127.0.0.1:7005",trace = true)
+@RpcApi(url = "127.0.0.1:7005",trace = "true")
 public interface PermissionApi {
     Permission permission(String username);
 }
