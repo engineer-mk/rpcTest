ea623be7cf4e024196e8ace39754b185b29a3681
mk <kuim123@qq.com>
2023-01-12 23:02:54 +0800

    分布式事务测试

--- provider/src/main/java/com/xmg/remoteApi/ProductApi.java
+++ provider/src/main/java/com/xmg/remoteApi/ProductApi.java
@@ -4,12 +4,15 @@ import com.xmg.entity.Product;
 import com.xmg.repository.ProductRepository;
 import io.seata.core.context.RootContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 import xmg.server.support.annotation.RpcProvider;
 
 import javax.persistence.EntityManager;
+import javax.persistence.LockModeType;
 
 /**
  * @author makui
@@ -17,19 +20,29 @@ import javax.persistence.EntityManager;
  **/
 @RpcProvider
 @Service
-@RequiredArgsConstructor
 public class ProductApi {
-    private final ProductRepository productRepository;
-    private final AccountApi accountApi;
-    private final EntityManager entityManager;
+    @Autowired
+    private  ProductRepository productRepository;
+    @Autowired
+    private  AccountApi accountApi;
+    @Autowired
+    private  EntityManager entityManager;
+    @Autowired
+    @Lazy
+    private  ProductApi ProductApi;
 
-    public void reduceCount(Long id, Long count) {
+    public void  reduceCount(Long id, Long count){
+        ProductApi.doReduceCount(id, count);
+        accountApi.deductBalance(1L, 10d * count);
+    }
+
+    @Transactional
+    public void doReduceCount(Long id, Long count) {
         final String xid = RootContext.getXID();
         System.out.println("xid:" + xid);
-        final Product product = entityManager.find(Product.class,id);
+        final Product product = entityManager.find(Product.class,id,LockModeType.PESSIMISTIC_WRITE);
         Assert.state(product.getCount() > count, "库存不足");
         product.setCount(product.getCount() - count);
         productRepository.save(product);
-        accountApi.deductBalance(1L, 10d * count);
     }
 }
